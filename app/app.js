4bdb08faa7eabd7710552d13068aa5b004643baa
NARajab <11211067@student.itk.ac.id>
2023-11-21 11:58:51 +0800

    add eslint and .prettierrc

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 module.exports = app;
