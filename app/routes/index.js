eddc33b4b90485fccc7686dc006f1c3327ccfc91
NARajab <11211067@student.itk.ac.id>
2023-11-29 21:48:32 +0800

    modify index router

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,17 @@
-const router = require('express').Router();
-const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('../../docs/swagger.json');
+const router = require('express').Router()
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = require('../../docs/swagger.json')
 
-router.use('/api-docs', swaggerUI.serve);
-router.use('/api-docs', swaggerUI.setup(swaggerDocument));
-const Welcome = require('./welcomeRouter');
-const Auth = require('./authRouter');
-const User = require('./userRouter');
-const Chapter = require('./chapterRouter.js');
+router.use('/api-docs', swaggerUI.serve)
+router.use('/api-docs', swaggerUI.setup(swaggerDocument))
+const Welcome = require('./welcomeRouter')
+const Auth = require('./authRouter')
+const User = require('./userRouter')
+const Chapter = require('./chapterRouter.js')
 
-router.use('/', Welcome);
-router.use('/api/v1/auth', Auth);
-router.use('/api/v1/user', User);
-router.use('/api/v1/chapters', Chapter);
+router.use('/welcome', Welcome)
+router.use('/api/v1/auth', Auth)
+router.use('/api/v1/user', User)
+router.use('/api/v1/chapter', Chapter)
 
-module.exports = router;
+module.exports = router
