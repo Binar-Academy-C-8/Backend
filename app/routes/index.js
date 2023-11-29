833a7d3f9e3148f4de5feea404928e1b0f4b69cd
NARajab <11211067@student.itk.ac.id>
2023-11-29 21:30:01 +0800

    modified:   app/routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,12 @@ const router = require('express').Router();
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('../../docs/swagger.json');
 
-// router file
-const Chapter = require('./chapterRouter.js');
-
 router.use('/api-docs', swaggerUI.serve);
 router.use('/api-docs', swaggerUI.setup(swaggerDocument));
 const Welcome = require('./welcomeRouter');
 const Auth = require('./authRouter');
 const User = require('./userRouter');
+const Chapter = require('./chapterRouter.js');
 
 router.use('/', Welcome);
 router.use('/api/v1/auth', Auth);
