d0a01d6f105915d9e91f35728e72b7fb740cd1a4
Ali Rajab <89598434+NARajab@users.noreply.github.com>
2023-12-01 23:09:05 +0800

    Delete Backend directory

--- Backend/app/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const User = require('../controller/userController');
-
-router.patch('/update/:id', User.updateUser);
-router.delete('/delete/:id', User.deleteUser);
-
-module.exports = router;
