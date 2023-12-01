624b3a844709c9e73d0ee771308cc45914c90d4b
NARajab <11211067@student.itk.ac.id>
2023-12-01 23:03:23 +0800

    modify structur

--- Backend/helper/compressVideo.js
+++
@@ -1,9 +0,0 @@
-const compressVideo = (data, newSize) => {
-  const resizedBuffer = data.buffer.slice(0, newSize);
-  data.size = newSize;
-  data.buffer = resizedBuffer;
-
-  return data;
-};
-
-module.exports = compressVideo;
--- app/controller/contentController.js
+++ app/controller/contentController.js
@@ -82,7 +82,16 @@ const insertContentByFile = async (req, res, next) => {
     const { chapterId } = req.params
     const videoBuffer = req.file.buffer
     const video = req.file
-    console.log(contentTitle)
+
+    const chapterData = await Chapter.findOne({
+      where: {
+        id: chapterId,
+      },
+    })
+
+    if (chapterData === null) {
+      return next(new ApiError('Chapter data is not found!', 404))
+    }
 
     const timeScale = videoBuffer.readUInt32BE(
       videoBuffer.indexOf(Buffer.from('mvhd')) + 16
@@ -248,7 +257,7 @@ const updateContentByFile = async (req, res, next) => {
       }
 
       res.status(200).json({
-        status: 'success',
+        status: 'Success',
         data: {
           updateContent: {
             updateContent,
