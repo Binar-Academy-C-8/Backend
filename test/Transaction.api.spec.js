4f6d928b124db96d4efa176165b67edf5e411e57
NirwanArr <arrachmann@gmail.com>
2023-12-29 00:37:50 +0700

    testing

--- test/Transaction.api.spec.js
+++ test/Transaction.api.spec.js
@@ -22,7 +22,7 @@ describe('API create transaction', () => {
     expect(loginResponse.body.message).toBe('Login berhasil');
 
     const userToken = loginResponse.body.data.token;
-// 
+
     const courseId = 6;
     const responseTransaction = await request(app)
       .post(`/api/v1/transaction/${courseId}`)
@@ -32,23 +32,6 @@ describe('API create transaction', () => {
     expect(responseTransaction.statusCode).toBe(201);
     expect(responseTransaction.body.status).toBe('Success');
   }, 10000);
-
-  it('Failed register because name is missing or empty', async () => {
-    const user = {
-      email: 'member_test@gmail.com',
-      password: '12345678',
-      phoneNumber: '01234567',
-      country: 'Indonesia',
-      city: 'Tangerang',
-    };
-
-    const response = await request(app)
-      .post('/api/v1/auth/member/register')
-      .send(user);
-
-    expect(response.statusCode).toBe(400);
-    expect(response.body.message).toBe('Nama tidak boleh kosong');
-  });
 
   it('Failed create transaction, order Id not found', async () => {
     const loginCredentials = {
